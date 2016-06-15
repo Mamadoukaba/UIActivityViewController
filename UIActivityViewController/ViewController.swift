489e0c322ecc7d13a058c23086512f9cd95eb9af
Mamadou Kaba <mamkab23@gmail.com>
2016-06-15 00:24:52 -0400

    Second Commit

--- UIActivityViewController/ViewController.swift
+++ UIActivityViewController/ViewController.swift
@@ -21,16 +21,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func shareButtonTapped(sender: AnyObject) {
         if shareTextField.text!.isEmpty {
+            
+            //Creating the message of the Alert View (if the Share Text Field if empty)
             let message = "Please enter some text to share to the world"
             
+            //Construction Alert View Controller
             let alertController = UIAlertController(title: "OOPPS!", message: message, preferredStyle: .Alert)
+            //Contruction Alert View Action (OK Button at the bottom of alert View)
             alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
             
+            //Presenting the Alert View Controller
             presentViewController(alertController, animated: true, completion: nil)
             
         } else {
+            //Constructing Activity View Controller
             activityViewController = UIActivityViewController(activityItems: [shareTextField.text! as NSString], applicationActivities: nil)
             
+            //Presenting Activity View Controller
             presentViewController(activityViewController, animated: true, completion: nil)
         }
         
